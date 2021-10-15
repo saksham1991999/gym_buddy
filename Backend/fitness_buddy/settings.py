6f39405d54af2cf25aad6bf0cdac508a201ec6fd
Saksham Mittal <181210045@nitdelhi.ac.in>
2021-10-15 17:52:38 +0530

    updated PostGIS Database

--- Backend/fitness_buddy/settings.py
+++ Backend/fitness_buddy/settings.py
@@ -98,14 +98,6 @@ else:
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
